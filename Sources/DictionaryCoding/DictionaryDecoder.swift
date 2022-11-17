f1b8c1d7e832fb8771e45b9d1d060dccc3cfd040
CHIBA Fumiya <fumiya.chiba@smartnews.com>
2022-11-17 11:32:19 +0900

    Make decode accept Any to handle Array or single primitive

--- Sources/DictionaryCoding/DictionaryDecoder.swift
+++ Sources/DictionaryCoding/DictionaryDecoder.swift
@@ -227,7 +227,7 @@ open class DictionaryDecoder {
     /// - returns: A value of the requested type.
     /// - throws: `DecodingError.dataCorrupted` if values requested from the payload are corrupted, or if the given data is not valid Dictionary.
     /// - throws: An error if any value throws an error during decoding.
-    open func decode<T : Decodable>(_ type: T.Type, from dictionary: NSDictionary) throws -> T {
+    open func decode<T : Decodable>(_ type: T.Type, from dictionary: Any) throws -> T {
         let decoder = _DictionaryDecoder(referencing: dictionary, options: self.options)
         guard let value = try decoder.unbox(dictionary, as: type) else {
             throw DecodingError.valueNotFound(type, DecodingError.Context(codingPath: [], debugDescription: "The given data did not contain a top-level value."))
@@ -235,23 +235,6 @@ open class DictionaryDecoder {
         
         return value
     }
-    
-    /// Decodes a top-level value of the given type from the given Dictionary representation.
-    ///
-    /// - parameter type: The type of the value to decode.
-    /// - parameter data: The data to decode from.
-    /// - returns: A value of the requested type.
-    /// - throws: `DecodingError.dataCorrupted` if values requested from the payload are corrupted, or if the given data is not valid Dictionary.
-    /// - throws: An error if any value throws an error during decoding.
-    open func decode<T : Decodable>(_ type: T.Type, from dictionary: [String:Any]) throws -> T {
-        let decoder = _DictionaryDecoder(referencing: dictionary, options: self.options)
-        guard let value = try decoder.unbox(dictionary, as: type) else {
-            throw DecodingError.valueNotFound(type, DecodingError.Context(codingPath: [], debugDescription: "The given data did not contain a top-level value."))
-        }
-        
-        return value
-    }
-    
 }
 
 // MARK: - _DictionaryDecoder
@@ -1364,6 +1347,6 @@ internal var _iso8601Formatter: ISO8601DateFormatter = {
 import Combine
 
 extension DictionaryDecoder: TopLevelDecoder {
-    public typealias Input = [String: Any]
+    public typealias Input = Any
 }
 #endif
